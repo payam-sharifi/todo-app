8f7a51fbc26fdb6b77763f703b8fe3ba191afd3c
payam <payam.shr@yahoo.com>
2025-11-09 11:48:15 +0100

    feat(backend):erstellte Ansichten und URLs

---
+++ server/todos_Api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import  path
+from rest_framework.routers import SimpleRouter
+from . import views
+
+router=SimpleRouter()
+router.register('todos',views.TodoViewSet,basename='todos')
+
+urlpatterns = router.urls
--- server/todos_Api/views.py
+++ server/todos_Api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Todo
+from .serializer import TodoSerializers
 
-# Create your views here.
+
+
+class TodoViewSet(ModelViewSet):
+    queryset=Todo.objects.all()
+    serializer_class=TodoSerializers
+    # lookup_field='id'
+  
+    def get_serializer_context(self):
+        return {'request':self.request}
+
+    def destroy(self,request,*args,**kwargs):
+        todo = get_object_or_404(Todo, pk=kwargs['pk'])
+        return super().destroy(request,*args,**kwargs)
+        self.perform_destroy(todo)
+        return Response(
+        {"message": "Todo deleted successfully"}, 
+        status=status.HTTP_204_NO_CONTENT)
