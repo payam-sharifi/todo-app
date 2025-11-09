d63e47187e48758674d47899b8dd0b9c7cced16e
payam <payam.shr@yahoo.com>
2025-11-09 11:36:22 +0100

    feat(backend):Die todo_api-App wurde erstellt und crossheaders sowie rest_framework wurden in den Einstellungen festgelegt und migriert.

--- server/todos_Api/models.py
+++ server/todos_Api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    STATUS_CHOICES = [
+        ('offen', 'Offen'),
+        ('in_bearbeitung', 'In Bearbeitung'),
+        ('erledigt', 'Erledigt'),
+    ]
+    titel = models.CharField(max_length=200)
+    beschreibung = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20, 
+        choices=STATUS_CHOICES, 
+        default='offen'
+    )
+    erstellt_am = models.DateTimeField(auto_now_add=True)
+    aktualisiert_am = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.titel
