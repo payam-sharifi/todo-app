63e1f13b0308be1c728a7300c6c870117040b677
payam <payam.shr@yahoo.com>
2025-11-09 21:48:47 +0100

    fix(backend):Behebung der Filterfunktionalität in TodoViewSet

--- server/todos_Api/views.py
+++ server/todos_Api/views.py
@@ -4,8 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.filters import OrderingFilter
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from .pagination import DefaultPagination
 from .models import Todo
@@ -14,22 +13,20 @@ from .filters import TodoFilter
 
 
 class TodoViewSet(ModelViewSet):
-    queryset=Todo.objects.all()
-    serializer_class=TodoSerializers
-    filter_backends=[DjangoFilterBackend,OrderingFilter,SearchFilter]
-    pagination_class=DefaultPagination
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    pagination_class = DefaultPagination
     filterset_class = TodoFilter
-    ordering_fields=['erstellt_am']
-    search_fields=['titel']
-    # lookup_field='id'
+    ordering_fields = ['erstellt_am']
+    search_fields = ['titel']
   
     def get_serializer_context(self):
-        return {'request':self.request}
+        return {'request': self.request}
 
-    def destroy(self,request,*args,**kwargs):
+    def destroy(self, request, *args, **kwargs):
         todo = get_object_or_404(Todo, pk=kwargs['pk'])
-        return super().destroy(request,*args,**kwargs)
-        self.perform_destroy(todo)
-        return Response(
-        {"message": "Todo deleted successfully"}, 
-        status=status.HTTP_204_NO_CONTENT)
+        
+       
+        return super().destroy(request, *args, **kwargs)
+        
