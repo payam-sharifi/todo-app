f36bad1ac6839e345a8560e6106dc1dbbddefb73
payam <payam.shr@yahoo.com>
2025-11-11 00:47:11 +0100

    fix(front):.env-Dateien hinzugefügt

--- client/src/lib/axios/axiosConfig.ts
+++ client/src/lib/axios/axiosConfig.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://127.0.0.1:8000/api/'
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
